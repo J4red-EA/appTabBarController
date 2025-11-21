7a5bc312a8ea1c8cf4c7973e33f6a22eab5c4258
Jared Esquivel <jaredesquivel@MacBook-Air-de-Jared.local>
2025-11-21 10:15:04 -0600

    Primer comit

---
+++ appTabBarController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  appTabBarController
+//
+//  Created by Jared Esquivel on 17/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ appTabBarController/vistaInicio.swift
@@ -0,0 +1,26 @@
+//
+//  vistaInicio.swift
+//  appTabBarController
+//
+//  Created by Jared Esquivel on 21/11/25.
+//
+
+import UIKit
+
+class vistaInicio: UIViewController {
+
+    
+
+    @IBOutlet weak var textoNombre: UITextField!
+    @IBAction func botonSaludar(_ sender: Any) {
+    }
+    @IBOutlet weak var labelSaludo: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
